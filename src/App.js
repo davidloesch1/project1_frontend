d006bac40fbafe60281f99e18ef31ae1caf10465
David <davidloesch1@gmail.com>
2020-08-06 22:11:16 -0700

    adding login for the frontend for regular login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const login = () => {
+    axios({
+      method: "post",
+      data: {
+        email: Email,
+        password: Password,
+      },
+      withCredentials: true,
+      url: "http://localhost:5000/register",
+    }).then((res) => console.log(res));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <a href="http://localhost:5000/auth/google">Login with Google</a>
+      <a href="http://localhost:5000/auth/facebook">Login with Facebook</a>
+      <form action="/login" method="post">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            name="username"
+            type="username"
+            className="form-control"
+            id="loginUsername"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            className="form-control"
+            id="loginPassword"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </form>
+      <div className="modal-footer">
+        <button
+          type="submit"
+          value="login"
+          className="btn btn-primary"
+          onClick={login}
+          data-dismiss="modal"
         >
-          Learn React
-        </a>
-      </header>
+          Login
+        </button>
+      </div>
     </div>
   );
 }
