b9b6837745ea403de651101d783d7805b49dacba
David <davidloesch1@gmail.com>
2020-08-08 11:28:58 -0700

    adjusting login with email and password

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
-import axios from "axios";
+import Axios from "axios";
 
 function App() {
+  //setting variable and state using hooks
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
+  const [data, setData] = useState(null);
+
+  //login function
   const login = () => {
-    axios({
-      method: "post",
+    console.log("login");
+    Axios({
+      method: "POST",
+      data: {
+        email: Email,
+        password: Password,
+      },
+      withCredentials: true,
+      url: "http://localhost:5000/auth/login",
+    }).then((res) => console.log(res));
+  };
+
+  //register function
+  const register = () => {
+    console.log("regiser");
+    Axios({
+      method: "POST",
       data: {
         email: Email,
         password: Password,
@@ -16,44 +35,60 @@ function App() {
       url: "http://localhost:5000/register",
     }).then((res) => console.log(res));
   };
-
+  const getUser = () => {
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:5000/user",
+    }).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+  };
+  const logout = () => {
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:5000/auth/logout",
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
   return (
     <div className="App">
       <a href="http://localhost:5000/auth/google">Login with Google</a>
       <a href="http://localhost:5000/auth/facebook">Login with Facebook</a>
-      <form action="/login" method="post">
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            name="username"
-            type="username"
-            className="form-control"
-            id="loginUsername"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            name="password"
-            type="password"
-            className="form-control"
-            id="loginPassword"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-      </form>
+      <div className="form-group">
+        <label htmlFor="username">Username</label>
+        <input
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
       <div className="modal-footer">
-        <button
-          type="submit"
-          value="login"
-          className="btn btn-primary"
-          onClick={login}
-          data-dismiss="modal"
-        >
+        <button className="btn btn-primary" onClick={login}>
           Login
         </button>
+        <button className="btn btn-primary" onClick={register}>
+          Register
+        </button>
+        <button className="btn btn-primary" onClick={getUser}>
+          Get Current Username
+        </button>
       </div>
+      {data ? (
+        <button className="btn btn-primary" onClick={logout}>
+          logout
+        </button>
+      ) : null}
     </div>
   );
 }
